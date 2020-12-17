26a9ad1ea41e6266f18d8509ee5b8fbc3e500036
kkufieta <katharina.kufieta@gmail.com>
2020-12-17 18:32:28 -0500

    Implement search function

--- src/quiz/cluster/kdtree.h
+++ src/quiz/cluster/kdtree.h
@@ -2,7 +2,10 @@
 // Quiz on implementing kd tree
 
 #include "../../render/render.h"
+#include <cmath>
 #include <iostream>
+#include <queue>
+#include <tuple>
 
 // Structure to represent node of kd tree
 struct Node {
@@ -22,10 +25,8 @@ struct KdTree {
 
   void insert(std::vector<float> point, int id) {
     Node **c = &root;
-    std::cout << c << std::endl;
     int level = 0;
     while (*c != NULL) {
-      std::cout << "level: " << level << ", c: " << c << std::endl;
       if (point[level % 2] < (*c)->point[level % 2]) {
         c = &(*c)->left;
       } else {
@@ -34,12 +35,32 @@ struct KdTree {
       level += 1;
     }
     *c = new Node(point, id);
-    std::cout << "inserted node: " << c << std::endl;
+  }
+
+  void searchHelper(std ::vector<float> target, Node *node, int level,
+                    float distanceTol, std::vector<int> &ids) {
+    if (node != NULL) {
+      if (node->point[0] >= target[0] - distanceTol &&
+          node->point[0] <= target[0] + distanceTol &&
+          node->point[1] >= target[1] - distanceTol &&
+          node->point[1] <= target[1] + distanceTol &&
+          std::sqrt(std::pow(node->point[0] - target[0], 2) +
+                    std::pow(node->point[1] - target[1], 2)) <= distanceTol) {
+        ids.push_back(node->id);
+      }
+      if (node->point[level % 2] >= target[level % 2] - distanceTol) {
+        searchHelper(target, node->left, level + 1, distanceTol, ids);
+      }
+      if (node->point[level % 2] <= target[level % 2] + distanceTol) {
+        searchHelper(target, node->right, level + 1, distanceTol, ids);
+      }
+    }
   }
 
   // return a list of point ids in the tree that are within distance of target
   std::vector<int> search(std::vector<float> target, float distanceTol) {
     std::vector<int> ids;
+    searchHelper(target, root, 0, distanceTol, ids);
     return ids;
   }
 };
